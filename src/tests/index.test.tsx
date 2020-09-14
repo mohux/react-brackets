5db35408b0d873553b20c963426f60438e4c3b12
mohux <57658626+mohux@users.noreply.github.com>
2020-09-14 15:32:14 +0300

    Simple testing added

---
+++ src/tests/index.test.tsx
@@ -0,0 +1,86 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { Bracket, RoundProps } from '..';
+import { render } from '@testing-library/react';
+
+test('Works with empty rounds', () => {
+  const rounds: RoundProps[] = [];
+  const { container } = render(<Bracket rounds={rounds} />);
+  expect(container).toBeTruthy();
+});
+
+test('Works with multiple rounds', () => {
+  const filledRounds: RoundProps[] = [
+    {
+      title: 'Round 1',
+      seeds: [
+        {
+          id: 1,
+          teams: [{ name: 'Team 1' }, { name: 'Team 2' }],
+        },
+        {
+          id: 2,
+          teams: [{ name: 'Team 3' }, { name: 'Team 4' }],
+        },
+      ],
+    },
+    {
+      title: 'Round 2',
+      seeds: [
+        {
+          id: 3,
+          teams: [{ name: 'Team 1' }, { name: 'Team 4' }],
+        },
+      ],
+    },
+  ];
+
+  const { getByText } = render(<Bracket rounds={filledRounds} />);
+
+  expect(getByText('Round 1')).toBeInTheDocument();
+  expect(getByText('Round 2')).toBeInTheDocument();
+});
+
+test('Works with custom seed', () => {
+  const filledRounds: RoundProps[] = [
+    {
+      title: 'Round 1',
+      seeds: [
+        {
+          id: 1,
+          teams: [{ name: 'Team 1' }, { name: 'Team 2' }],
+        },
+        {
+          id: 2,
+          teams: [{ name: 'Team 3' }, { name: 'Team 4' }],
+        },
+      ],
+    },
+    {
+      title: 'Round 2',
+      seeds: [
+        {
+          id: 3,
+          teams: [{ name: 'Team 1' }, { name: 'Team 4' }],
+        },
+      ],
+    },
+  ];
+
+  const RenderSeed = (seed: any) => {
+    return (
+      <div>
+        <div>{seed.teams[0]?.name}</div>
+        <span>VS</span>
+        <div>{seed.teams[1]?.name}</div>
+      </div>
+    );
+  };
+
+  const { getAllByText, getByText } = render(<Bracket rounds={filledRounds} renderSeedComponent={RenderSeed} />);
+
+  expect(getAllByText('VS')[0]).toBeInTheDocument();
+  // checking that Team 1 was rendered twice
+  expect(getAllByText('Team 1').length).toBe(2);
+  expect(getByText('Team 2')).toBeInTheDocument();
+});
