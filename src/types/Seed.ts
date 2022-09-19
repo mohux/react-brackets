ccc9cd016217ab2be6df54bd6f3e94c82545393e
Leandro <leandrofontellas@gmail.com>
2022-09-19 19:36:17 -0300

    fix: adding alternative type for id

--- src/types/Seed.ts
+++ src/types/Seed.ts
@@ -1,7 +1,7 @@
 import { IRoundProps } from './Rounds';
 
 export type ISeedProps = {
-  id: number;
+  id: number | string;
   teams: Array<{ name?: string; [key: string]: any }>;
   date?: string;
   mobileBreakpoint?: number;
