98efb498f595517dba3a301e1df424c612b33c5f
oliverLooney <oliverjlooney@gmail.com>
2023-07-05 21:39:24 +0100

    added two sided bracket support for SingleElimination

--- src/brackets/index.tsx
+++ src/brackets/index.tsx
@@ -4,6 +4,8 @@ import SwipeableViews from 'react-swipeable-views';
 import useMedia from '../hooks/useMedia';
 import { renderSeed, renderTitle } from '../utils/renders';
 import { ISingleEliminationProps } from '../types/SingleElimination';
+import { IRoundProps } from '../types/Rounds';
+import {ISeedProps} from "../types/Seed";
 
 const SingleElimination = ({
   rounds,
@@ -12,6 +14,7 @@ const SingleElimination = ({
   bracketClassName,
   swipeableProps = {},
   mobileBreakpoint = 992,
+  twoSided = false,
   renderSeedComponent = renderSeed,
   roundTitleComponent = renderTitle,
 }: ISingleEliminationProps) => {
@@ -23,21 +26,23 @@ const SingleElimination = ({
       {round.title && roundTitleComponent(round.title, roundIdx)}
       <SeedsList>
         {round.seeds.map((seed, idx) => {
-          return (
-            <Fragment key={seed.id}>
-              {renderSeedComponent({
-                seed,
-                breakpoint: mobileBreakpoint,
-                roundIndex: roundIdx,
-                seedIndex: idx,
-                rounds,
-              })}
-            </Fragment>
-          );
+          return getFragment(seed, roundIdx, idx, rounds, false)
         })}
       </SeedsList>
     </Round>
   ));
+
+  const getFragment = (seed: ISeedProps, roundIdx: number, idx: number, rounds: IRoundProps[], isMiddleOfTwoSided: any) =>
+    <Fragment key={seed.id}>
+      {renderSeedComponent({
+        seed,
+        breakpoint: mobileBreakpoint,
+        roundIndex: roundIdx,
+        seedIndex: idx,
+        rounds,
+        isMiddleOfTwoSided
+      })}
+    </Fragment>;
 
   if (isResponsive) {
     // Since SwipeableViewsProps have an issue that it uses ref inside of it, We need to remove ref from the object
@@ -50,6 +55,37 @@ const SingleElimination = ({
       </Bracket>
     );
   }
+
+  const getRenderedRounds = (
+    roundsStartIndex: number,
+    roundsEndIndex: number,
+    renderFirstHalfOfRoundsSeeds: boolean,
+    rounds: IRoundProps[],
+    dir: string) =>
+    rounds.slice(roundsStartIndex, roundsEndIndex).map((round, roundIdx) => (
+      <Round key={round.title} className={roundClassName} mobileBreakpoint={mobileBreakpoint}>
+        {round.title && roundTitleComponent(round.title, roundIdx)}
+        <SeedsList dir={dir}>
+          {renderFirstHalfOfRoundsSeeds
+            ? round.seeds.slice(0, round.seeds.length / 2).map((seed, idx) => getFragment(seed, roundIdx, idx, rounds, false))
+            : round.seeds.slice(round.seeds.length / 2, round.seeds.length).map((seed, idx) => (getFragment(seed, roundIdx, idx, rounds, roundIdx < roundsEndIndex - 2 ? true : false))
+              )}
+        </SeedsList>
+      </Round>
+    ));
+
+  if (twoSided) {
+    return (
+      <Bracket className={bracketClassName} mobileBreakpoint={mobileBreakpoint}>
+        {[
+          getRenderedRounds(0, rounds.length - 1, true, rounds, "ltr"),
+          getRenderedRounds(rounds.length - 1, rounds.length, false, rounds, "twoSided"),
+          getRenderedRounds(1, rounds.length, false, [...rounds].reverse(), "rtl"),
+        ]}
+      </Bracket>
+    );
+  }
+
   return (
     <Bracket className={bracketClassName} dir={rtl ? 'rtl' : 'ltr'} mobileBreakpoint={mobileBreakpoint}>
       {data}
